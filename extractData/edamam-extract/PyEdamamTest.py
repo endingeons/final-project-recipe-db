c396d1566f13ad51d62540ff4cff5d9012a4d88c
Philip Franco <pcfranco05@gmail.com>
2023-04-26 23:08:57 -0400

    Convert Data To pandas
    Converts all the data to a pandas dataframe. I still need to find where serving size is and organize the ingredients and measurements. right now they are just a string in the pandas array

--- extractData/edamam-extract/PyEdamamTest.py
+++ extractData/edamam-extract/PyEdamamTest.py
@@ -5,39 +5,120 @@ import pandas as pd
 e = PyEdamam(recipes_appid=appId,
            recipes_appkey=appKey)
 
-for recipe in e.search_recipe("Pinapple Ham"):
-  print(recipe)
-  #print(recipe.calories)
-  #print(recipe.cautions, recipe.dietLabels, recipe.healthLabels)
+# Test Loop through data to get information from a single recipe.
+def getFoodInfo(search, df_recipeBook, ingredientNum):
+  df_recipe =  pd.DataFrame()
+
+  for recipe in e.search_recipe(search):
+    print(recipe)
+    
+    ingredientNum = ingredientNum + 1
+    website = recipe.url
+    image = recipe.image
+    recipeTitle = recipe.label
+    servingSize = -1
+    foodGenre = recipe.cuisineType
+    calories = recipe.calories
+    time = recipe.totalTime
+
+    fat =  recipe.digest['Fat']['daily']
+    protein =  recipe.digest['Protein']['daily']
+    cholesterol =  recipe.digest['Cholesterol']['daily']
+    sodium =  recipe.digest['Sodium']['daily']
+    carbs =  recipe.digest['Carbs']['daily']
+   
+    parseInfo = recipe.totalDaily
+    # obtain the saturated fat percentage
+    if parseInfo[2].label == 'Saturated':
+      satFat = parseInfo[2].quantity
+    else:
+      satFat = -1
+      for num in parseInfo:
+        if num.label == 'Saturated':
+          satFat = num.quantity
+          break
+    
+    parseInfo = recipe.totalNutrients
+    # obtain the sugar percentage
+    if parseInfo[9].label == 'Sugars':
+      sugar = parseInfo[2].quantity
+    else:
+      sugar = -1
+      for num in parseInfo:
+        if num.label == 'Sugars':
+          sugar = num.quantity
+          break
+
+    # dietary restriciton toggles
+    dietaryRestrictions = recipe.healthLabels
+
+    isVegan = False
+    isVegetarian = False
+    isPescatarian = False
+    isDairyFree = False
+    isGlutenFree = False
+    isPeanutFree = False
+
+    for restrict in dietaryRestrictions:
+      if restrict == 'Vegan':
+        isVegan = True
+      elif restrict == 'Vegetarian':
+        isVegetarian = True
+      elif restrict == 'Pescatarian':
+        isPescatarian = True
+      elif restrict == 'Dairy-Free':
+        isDairyFree = True
+      elif restrict == 'Gluten-Free':
+        isGlutenFree = True
+      elif restrict == 'Peanut-Free':
+        isPeanutFree = True
+
+    ingredientNames = recipe.ingredient_names
+    ingredientQuantities = recipe.ingredient_quantities
+    
+    df_recipe['recipeTitle'] = recipeTitle
+    df_recipe['recipeSearch_Used'] = search
+    df_recipe['website'] = website 
+    df_recipe['image'] = image
+    df_recipe['servingSize'] = servingSize
+    df_recipe['foodGenre'] = foodGenre
+    df_recipe['calories'] = calories
+    df_recipe['time'] = time
+
+    df_recipe['fat'] = fat
+    df_recipe['satFat'] = satFat
+    df_recipe['protein'] = protein
+    df_recipe['cholesterol'] = cholesterol
+    df_recipe['sodium'] = sodium
+    df_recipe['carbs'] = carbs
+    df_recipe['sugar'] = sugar
+
+    df_recipe['isVegan'] = isVegan
+    df_recipe['isVegetarian'] = isVegetarian
+    df_recipe['isPescatarian'] = isPescatarian
+    df_recipe['isDairyFree'] = isDairyFree
+    df_recipe['isGlutenFree'] = isGlutenFree
+    df_recipe['isPeanutFree'] = isPeanutFree
   
-  URL = recipe.url
-  recipeTitle = recipe
-  servingSize = 'nan'
-  foodGenre = recipe.cuisineType
-  calories = recipe.calories
-  time = recipe.totalTime
-
-  #saturated fats
-  fat =  recipe.digest['Fat']['total']
-  fatU =  recipe.digest['Fat']['unit']
+    df_recipe['ingredientNames'] = [ingredientNames]
+    df_recipe['ingredientQuantities'] = [ingredientQuantities]
   
-  protein =  recipe.digest['Protein']['total']
-  protienU =  recipe.digest['Protein']['unit']
-
-  cholesterol =  recipe.digest['Cholesterol']['total']
-  cholesterolU =  recipe.digest['Cholesterol']['unit']
+    df_recipeBook = df_recipeBook.append(df_recipe)
   
-  sodium =  recipe.digest['Sodium']['total']
-  sodiumU =  recipe.digest['Sodium']['unit']
+  return df_recipeBook, ingredientNum
 
-  # Carbs?? might be regular fats?
-  carbs =  recipe.digest['Carbs']['total']
-  carbsU =  recipe.digest['Carbs']['unit']
 
-  dietaryRestrictions =  recipe.healthLabels
+# list of foods
+listOfFoods = ['Chicken Parmesan', 'Cheeseburger', 'Ham', 'Scrambled Eggs', 'Egg Sandwich', 'Bread', 'Pizza', 'Pasta', 'Sweet Tea']
+df =  pd.DataFrame()
 
-  ingrediantNames = recipe.ingredient_names
-  ingrediantQuantities = recipe.ingredient_quantities
+# used a large number so i dont overlap with Chelsea
+ingredientNum = 10000
+
+for food in listOfFoods:
+  df = getFoodInfo(food, df, ingredientNum)
+
+
 
 # for testing purposes
   #print(recipe.url)
@@ -72,7 +153,7 @@ Ingredient List
 Recipy Nutrition
    Recipy Key
    Fats              ---
-   Saturated Fats
+   Saturated Fats    ---
    Protein           ---
    Cholesterol       ---
    Sugar 
